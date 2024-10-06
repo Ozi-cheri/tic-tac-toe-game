2cdfc0b54c244d71f08d7a581c29c2427a783769
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-10-06 16:28:52 +0000

    Modify script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
-// Game state variables
 let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let gameOver = false;
-let scores = { X: 0, O: 0 };
+let scores = {
+    X: 0,
+    O: 0
+};
 
-// Winning combinations vertically
 const winCombos = [
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [0, 4, 8], 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
     [2, 4, 6]
 ];
 
